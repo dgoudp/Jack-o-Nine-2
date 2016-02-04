3d1a1d91d82c6cec5e5f29d7adc075a9bf40eac8
Gfurst <gwfurst@gmail.com>
2016-02-03 23:50:13 -0200

    initial draft on test screen for girls

---
+++ Game/Main/girlLayered.gd
@@ -0,0 +1,16 @@
+####	Layer girl panel
+#
+#		simple panel to test the build of layered girls
+#
+
+extends Node
+
+var libs = preload("res://Game/Main/libs.gd")
+
+var girlStat = {}
+
+func _ready():
+	
+	pass
+
+
